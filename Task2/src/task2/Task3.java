db6c2433efc2debde8836bd1a5e848d86728d276
HafizMohsinRaza <81870281+HafizMohsinRaza@users.noreply.github.com>
2021-04-24 16:03:25 +0500

    Task3(b)

--- Task2/src/task2/Task3.java
+++ Task2/src/task2/Task3.java
@@ -1,8 +1,35 @@
-public class Task3{
-  public static void main (String [] args)
-  { 
-    String str = " The quick brown fox jump. ";
-    String ridMultipleblank = str.replaceAll("\\s","");
-    System.out.println(ridMultipleblank);
+
+class Task3 {
+
+ 
+ public static String removeInteger(String s)
+ {
+  
+  char[] charArray = s.toCharArray();
+  String result = "";
+
+  // Traverse the character array
+  for (int i = 0; i < charArray.length; i++) {
+
+   // Check if the specified character is not digit
+
+   if (!Character.isDigit(charArray[i])) {
+    result = result + charArray[i];
+   }
   }
+
+ 
+  return result;
+ }
+
+ // Driver Co
+ public static void main(String args[])
+ {
+
+  // Given alphanumeric string str
+  String s = "He is78 a go23od Prog97ram08er!";
+
+  
+  System.out.println(removeInteger(s));
+ }
 }
